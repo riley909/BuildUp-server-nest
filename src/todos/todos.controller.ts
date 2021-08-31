2faa032c92ebea6471e6c4d262b7261a1f7ba2e0
riley909 <increaseblue666@gmail.com>
2021-08-31 23:48:03 +0900

    add constructor with DI

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { TodosService } from './todos.service';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  constructor(private todosService: TodosService) {}
+}

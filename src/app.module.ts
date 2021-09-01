d7c101aa1c7b5d49458c35b60a56e2b2024c4e90
riley909 <increaseblue666@gmail.com>
2021-09-01 10:37:10 +0900

    add ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [TodosModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
+    TodosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

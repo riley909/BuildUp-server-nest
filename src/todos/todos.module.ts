6aa542aaf2423b944ea9e852fa8c43e47acdf955
riley909 <increaseblue666@gmail.com>
2021-09-02 00:30:31 +0900

    add ConfigModule

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [TodosController],
   providers: [TodosService],
 })

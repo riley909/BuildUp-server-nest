a73694f2c3b9bc4e75ac772c37eb7cbfa865956a
riley909 <increaseblue666@gmail.com>
2021-09-08 12:46:25 +0900

    add OneToMany to Todo

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Todo } from 'src/todos/todo.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
@@ -13,4 +14,7 @@ export class User {
 
   @Column()
   password: string;
+
+  @OneToMany((_type) => Todo, (todo) => todo.user)
+  todos: Todo[];
 }

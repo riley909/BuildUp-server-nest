4180742755f0a6199345bfd4ec2f2208e4805fa6
riley909 <increaseblue666@gmail.com>
2021-09-08 12:46:46 +0900

    add ManyToOne to User

--- src/todos/todo.entity.ts
+++ src/todos/todo.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/auth/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Todo {
@@ -14,6 +15,6 @@ export class Todo {
   @Column()
   isChecked: boolean;
 
-  // foreignKey: 'user_id'
-  // foreignKey: 'date_id'
+  @ManyToOne((_type) => User, (user) => user.todos, { eager: false })
+  user: User;
 }

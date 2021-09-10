db9a258060baf852aab843114e4759c07e18a5f6
riley909 <increaseblue666@gmail.com>
2021-09-11 01:00:32 +0900

    add validators

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,5 +1,25 @@
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is too weak',
+  })
   password: string;
 }

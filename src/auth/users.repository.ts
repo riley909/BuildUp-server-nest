40c1268b36b9bcad3541fd473b521e27196b3cc8
riley909 <increaseblue666@gmail.com>
2021-09-10 23:50:41 +0900

    add createUser

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,5 +1,17 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UsersRepository extends Repository<User> {}
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { email, username, password } = authCredentialsDto;
+
+    const user = this.create({
+      email,
+      username,
+      password,
+    });
+    await this.save(user);
+  }
+}

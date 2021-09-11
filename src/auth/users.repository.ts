ac97ac76fdeea801c037d182d4ea65146b3106e3
riley909 <increaseblue666@gmail.com>
2021-09-11 13:49:53 +0900

    add try-catch for ConfictException

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -2,6 +2,10 @@ import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 import * as bcrypt from 'bcrypt';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
@@ -16,6 +20,14 @@ export class UsersRepository extends Repository<User> {
       username,
       password: hashedPwd,
     });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Email already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }

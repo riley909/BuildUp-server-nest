b7eb5421b1090b7cbbcc385385fd4863e1152152
riley909 <increaseblue666@gmail.com>
2021-09-11 16:07:44 +0900

    import TypeOrmModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersRepository } from './users.repository';
 
 @Module({
   imports: [
@@ -13,6 +15,7 @@ import { AuthService } from './auth.service';
         expiresIn: 3600,
       },
     }),
+    TypeOrmModule.forFeature([UsersRepository]),
   ],
   controllers: [AuthController],
   providers: [AuthService],

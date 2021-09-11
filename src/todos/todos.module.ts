43f5958817c9d2ff0fd42df08509bbe17046f91d
riley909 <increaseblue666@gmail.com>
2021-09-11 18:27:40 +0900

    import TypeOrmModule

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodosController } from './todos.controller';
+import { TodosRepository } from './todos.repository';
 import { TodosService } from './todos.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [TypeOrmModule.forFeature([TodosRepository]), ConfigModule],
   controllers: [TodosController],
   providers: [TodosService],
 })

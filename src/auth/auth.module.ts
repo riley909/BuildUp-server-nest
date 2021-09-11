d7426a122d0866c47d24dfdc4160d45fce76f564
riley909 <increaseblue666@gmail.com>
2021-09-11 16:02:28 +0900

    import PassportModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [AuthController],
   providers: [AuthService],
 })

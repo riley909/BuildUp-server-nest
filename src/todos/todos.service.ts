fe0510af56ee3b672d1a01240de2e6cd125d6230
riley909 <increaseblue666@gmail.com>
2021-09-14 12:01:15 +0900

    add getTodos

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/user.entity';
+import { Todo } from './todo.entity';
 import { TodosRepository } from './todos.repository';
 
 @Injectable()
@@ -8,4 +10,7 @@ export class TodosService {
     @InjectRepository(TodosRepository)
     private todosRepository: TodosRepository,
   ) {}
+  getTodos(user: User): Promise<Todo[]> {
+    return this.todosRepository.getTodos(user);
+  }
 }

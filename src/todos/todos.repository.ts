05019de5c0e4946af7d9752f7d237028b5517257
riley909 <increaseblue666@gmail.com>
2021-09-14 12:01:01 +0900

    add getTodos

--- src/todos/todos.repository.ts
+++ src/todos/todos.repository.ts
@@ -1,5 +1,14 @@
+import { User } from 'src/auth/user.entity';
 import { EntityRepository, Repository } from 'typeorm';
 import { Todo } from './todo.entity';
 
 @EntityRepository(Todo)
-export class TodosRepository extends Repository<Todo> {}
+export class TodosRepository extends Repository<Todo> {
+  async getTodos(user: User): Promise<Todo[]> {
+    const query = this.createQueryBuilder('todo');
+    query.where({ user });
+
+    const todos = await query.getMany();
+    return todos;
+  }
+}

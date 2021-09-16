c61920961e14e9744e20a47943291485eca60a75
riley909 <increaseblue666@gmail.com>
2021-09-16 23:25:29 +0900

    fix CreateTodoDto

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,25 +1,10 @@
-import {
-  IsBoolean,
-  IsDate,
-  IsNotEmpty,
-  IsNumber,
-  IsString,
-} from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateTodoDto {
   @IsNotEmpty()
   @IsString()
   content: string;
 
-  @IsNotEmpty()
-  @IsDate()
-  created: Date;
-
-  @IsNotEmpty()
   @IsNumber()
   order: number;
-
-  @IsNotEmpty()
-  @IsBoolean()
-  isChecked: boolean;
 }

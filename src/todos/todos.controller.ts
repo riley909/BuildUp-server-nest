8e99c84ae366db3cc71848ac244c22e41776b3fe
riley909 <increaseblue666@gmail.com>
2021-09-16 23:27:20 +0900

    add createTodo

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from 'src/auth/user.entity';
+import { CreateTodoDto } from './dto/create-todo.dto';
 import { Todo } from './todo.entity';
 import { TodosService } from './todos.service';
 
@@ -13,5 +14,13 @@ export class TodosController {
   @Get()
   getTodos(@GetUser() user: User): Promise<Todo[]> {
     return this.todosService.getTodos(user);
+  }
+
+  @Post()
+  createTodo(
+    @Body() createTodoDto: CreateTodoDto,
+    @GetUser() user: User,
+  ): Promise<Todo> {
+    return this.todosService.createTodo(createTodoDto, user);
   }
 }

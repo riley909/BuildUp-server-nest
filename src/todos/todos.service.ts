064d3795833d9ab378f11b5bd7cbbe6e21387150
riley909 <increaseblue666@gmail.com>
2021-09-16 23:27:05 +0900

    add createTodo

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/auth/user.entity';
+import { CreateTodoDto } from './dto/create-todo.dto';
 import { Todo } from './todo.entity';
 import { TodosRepository } from './todos.repository';
 
@@ -12,5 +13,9 @@ export class TodosService {
   ) {}
   getTodos(user: User): Promise<Todo[]> {
     return this.todosRepository.getTodos(user);
+  }
+
+  createTodo(createTodoDto: CreateTodoDto, user: User): Promise<Todo> {
+    return this.todosRepository.createTodo(createTodoDto, user);
   }
 }

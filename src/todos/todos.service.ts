b2b174bf1e69960599120dacb37a4127247c9eec
riley909 <increaseblue666@gmail.com>
2021-09-18 18:45:36 +0900

    add updateStatus

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/auth/user.entity';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateContentDto } from './dto/update-content.dto';
+import { UpdateStatusDto } from './dto/update-status.dto';
 import { Todo } from './todo.entity';
 import { TodosRepository } from './todos.repository';
 
@@ -34,5 +35,13 @@ export class TodosService {
     user: User,
   ): Promise<Todo> {
     return this.todosRepository.updateContent(id, updateContentDto, user);
+  }
+
+  updateStatus(
+    id: number,
+    updateStatusDto: UpdateStatusDto,
+    user: User,
+  ): Promise<Todo> {
+    return this.todosRepository.updateStatus(id, updateStatusDto, user);
   }
 }

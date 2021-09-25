40e189413ed5f3bd4253762dbcc450d6101404cb
riley909 <increaseblue666@gmail.com>
2021-09-25 17:16:01 +0900

    add getTodos test

--- src/todos/todos.service.spec.ts
+++ src/todos/todos.service.spec.ts
@@ -1,1 +1,42 @@
-describe('TodosService', () => {});
+import { Test } from '@nestjs/testing';
+import { TodosRepository } from './todos.repository';
+import { TodosService } from './todos.service';
+
+const mockTodosRepository = () => ({
+  getTodos: jest.fn(),
+});
+
+const mockUser = {
+  email: 'testemail',
+  id: 1,
+  username: 'testusername',
+  password: 'testpassword',
+  todos: [],
+};
+
+describe('TodosService', () => {
+  let todosService: TodosService;
+  let todosRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        TodosService,
+        { provide: TodosRepository, useFactory: mockTodosRepository },
+      ],
+    }).compile();
+
+    todosService = module.get(TodosService);
+    todosRepository = module.get(TodosRepository);
+  });
+
+  describe('getTodos', () => {
+    it('TodosRepository.getTodos를 호출하고 결과를 반환합니다.', () => {
+      expect(todosRepository.getTodos).not.toHaveBeenCalled();
+
+      todosRepository.getTodos.mockResolvedValue('some value');
+      const result = todosService.getTodos(mockUser);
+      expect(result).toEqual('some value');
+    });
+  });
+});

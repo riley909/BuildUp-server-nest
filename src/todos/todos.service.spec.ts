fd61f00c80855926704bbb3d55eb13110fc44d7d
riley909 <increaseblue666@gmail.com>
2021-09-25 18:09:04 +0900

    add getTodoById test

--- src/todos/todos.service.spec.ts
+++ src/todos/todos.service.spec.ts
@@ -1,9 +1,11 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { TodosRepository } from './todos.repository';
 import { TodosService } from './todos.service';
 
 const mockTodosRepository = () => ({
   getTodos: jest.fn(),
+  findOne: jest.fn(),
 });
 
 const mockUser = {
@@ -37,6 +39,29 @@ describe('TodosService', () => {
       todosRepository.getTodos.mockResolvedValue('some value');
       const result = todosService.getTodos(mockUser);
       expect(result).toEqual('some value');
+    });
+  });
+
+  describe('getTodosById', () => {
+    it('TodosRepository.findOne를 호출하고 결과를 반환합니다.', async () => {
+      const mockTodo = {
+        id: 1,
+        content: 'test content',
+        created: '2021-09-01',
+        order: 1,
+        isChecked: false,
+      };
+
+      todosRepository.findOne.mockResolvedValue(mockTodo);
+      const result = await todosService.getTodoById(1, mockUser);
+      expect(result).toEqual(mockTodo);
+    });
+
+    it('TodosRepository.findOne을 호출하고 에러를 처리합니다.', async () => {
+      todosRepository.findOne.mockResolvedValue(null);
+      expect(todosService.getTodoById(1, mockUser)).rejects.toThrow(
+        NotFoundException,
+      );
     });
   });
 });
